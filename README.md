fb0c6352fa5e97545e9ebf94e2156735bda7af01
chenxm <chenxm@wpt-vm.(none)>
2012-12-28 18:00:46 +0800

    output tcp flows without SYN

--- README.md
+++ README.md
@@ -1,27 +1,37 @@
 pkt2flow
 ========
 
-by chenxm, SJTU
+by chenxm, Shanghai Jiao Tong Univ.
+chenxm35@gmail.com
 2012-12
 
-To seperate the packets into flows (UDP or TCP) by 4-tuple of (src_ip, dst_ip, src_port, dst_port).
-Each flow will be saved into a pcap file named with 4_tuple and the timestamp of
-the first packet of the flow for further analysis, e.g., using tcpflow, tcptrace, captcp.py etc.
+A simple utility to classify packets into flows. It's so simple that only one task
+is aimed to finish.
 
-The packets are saved in the time order without any processing like TCP resembling.
+For Deep Packet Inspection or flow classification, it's so common to analyze the
+feature of one specific flow. I have make the attempt to use made-ready tools like
+`tcpflows`, `tcpslice`, `tcpsplit`, but all these tools try to either decrease the
+trace volume (under requirement) or resemble the packets into flow payloads (over
+requirement). I have not found a simple tool to classify the packets into flows without
+further processing. This is why this program is born.
 
-The flow timeout is considered as 30 minutes which can be changed in pkt2flow.h.
+The inner function of this program behaves using the 4-tuple (src_ip, dst_ip, src_port, dst_port)
+to seperate the packets into TCP or UDP flows. Each flow will be saved into a pcap 
+file named with 4-tuple and the timestamp of the first packet of the flow. The packets are 
+saved in the order as read from the source. Any further processing like TCP resembling is
+not performed. The flow timeout is considered as 30 minutes which can be changed in pkt2flow.h.
+
 
 Usage
 --------
 
 run `scons` to compile.
 
-usage: ./pkt2flow [-u] -o outdir pcapfile
+Usage: ./pkt2flow [-huv] [-o outdir] pcapfile
 
-  options:
-  
-    -h        usage instructions
-    -v        dump the validated TCP flows only with the first SYN detected
-    -u        dump UDP flows
-    -o        Output directory
+Options:
+	-h	print this help and exit
+	-u	also dump (U)DP flows
+	-v	also dump the in(v)alid TCP flows without the SYN option
+	-o	(o)utput directory
+
--- pkt2flow.c
+++ pkt2flow.c
@@ -35,9 +35,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <net/if.h>
 #include <net/ethernet.h>
 #include <netinet/in.h>
@@ -49,24 +51,27 @@
 #include <pcap.h>
 #include "pkt2flow.h"
 
-char *readfile = NULL;
-char *outputdir = NULL;
-char tcpsyn = 0;
+char tcpsyn = 1;
 char dumpudp = 0;
+char *readfile = NULL;
+//char *interface = NULL;
+char *outputdir = "outdir";
+char outputpath[PATH_NAME_LENGTH];
 pcap_t *inputp = NULL;
 struct ip_pair *pairs [HASH_TBL_SIZE];
-char *flownames = "flow_names.txt";
 
 void usage (char *progname)
 {
-	fprintf (stderr,"Usage: %s [-u] -o outdir pcapfile\n", progname);
-	fprintf (stderr,"\nTo seperate the packets into flows (UDP or TCP) by 4-tuple of (src_ip, dst_ip, src_port, dst_port). Each flow will be saved into a pcap file named with 4_tuple and the timestamp of the first packet of the flow. The flow timeout is treated as 30 minutes.\n");
-	fprintf (stderr,"\nOptions:\n");
-	fprintf (stderr,"	-h	usage instructions\n");
-	fprintf (stderr,"	-v	dump the validated TCP flows only with the first SYN detected\n");
-	fprintf (stderr,"	-u	dump UDP flows\n");
-	fprintf (stderr,"	-o	output directory\n");
-	exit (1);
+	fprintf (stderr,"Name: %s\n", __GLOBAL_NAME__);
+	fprintf (stderr,"Version: %s\n", __VERSION__);
+	fprintf (stderr,"Author: %s\n", __AUTHOR__);
+	fprintf (stderr,"Program to seperate the packets into flows (UDP or TCP).\n\n");
+	fprintf (stderr,"Usage: %s [-huv] [-o outdir] pcapfile\n\n", progname);
+	fprintf (stderr,"Options:\n");
+	fprintf (stderr,"	-h	print this help and exit\n");
+	fprintf (stderr,"	-u	also dump (U)DP flows\n");
+	fprintf (stderr,"	-v	also dump the in(v)alid TCP flows without the SYN option\n");
+	fprintf (stderr,"	-o	(o)utput directory\n");
 }
 
 
@@ -86,7 +91,7 @@ void parseargs (int argc,char *argv[])
 				dumpudp = 1;
 				break;
 			case 'v':
-				tcpsyn = 1;
+				tcpsyn = 0;
 				break;
 			default:
 				usage(argv [0]);
@@ -96,19 +101,11 @@ void parseargs (int argc,char *argv[])
 	
 	if (optind < argc)
 		readfile = argv[optind];
-	if((readfile == NULL) || (outputdir == NULL))
+	if (readfile == NULL){
+		fprintf (stderr, "pcap file not given\n");
 		usage(argv[0]);
-	if(access(outputdir, F_OK) != 0){
-		fprintf(stderr, "output folder dost not exist.\n");
-		exit(-1);	
+		exit (1);
 	}
-}
-
-void record_flow_name(char *fname){
-	FILE *f = fopen(flownames, "a+");
-	fputs(fname, f);
-	fputc('\n', f);
-	fclose(f);
 }
 
 void open_trace_file ()
@@ -121,6 +118,47 @@ void open_trace_file ()
 	}
 }
 
+char *resemble_file_path(struct pkt_dump_file *pdf){
+	char *cwd = getcwd(NULL, 0);	// backup the current working directory
+	char *folder = NULL;
+	char *dupPath = NULL;
+	int check;
+	DIR *dirPtr = NULL;
+	struct stat statBuff;
+
+	strcpy(outputpath, outputdir);
+	strcat(outputpath, "/");
+	if (pdf->status == STS_TCP_SYN)
+		strcat(outputpath, "tcp_syn/");	
+	else if (pdf->status == STS_TCP_NOSYN)
+		strcat(outputpath, "tcp_nosyn/");
+	else if (pdf->status == STS_UDP)
+		strcat(outputpath, "udp/");
+	else
+		strcat(outputpath, "others/");
+
+	// Check the path folder and create the folders if they are not there
+	dupPath = strdup(outputpath);
+	if(!(stat(dupPath, &statBuff) != -1 && S_ISDIR(statBuff.st_mode))) {
+		folder = strtok(dupPath, "/");
+		while (folder != NULL){
+			if (!(stat(folder, &statBuff) != -1 && S_ISDIR(statBuff.st_mode))){
+				check = mkdir(folder, S_IRWXU);
+				if (check != 0){
+					fprintf(stderr, "making directory error: %s\n", dupPath);
+					exit(-1);
+				}
+			}
+			chdir(folder);
+			folder = strtok(NULL, "/");
+		}
+	}
+	chdir(cwd);
+	free(cwd);
+	free(dupPath);
+	strcat(outputpath, pdf->file_name);
+	return outputpath;
+}
 
 void process_trace ()
 {
@@ -129,18 +167,14 @@ void process_trace ()
 	struct ip *iph = NULL;
 	struct tcphdr *tcph = NULL;
 	struct udphdr *udph = NULL;
-	u_char *pkt = NULL;
-	struct pkt_dump_file *pdf = NULL;
 	struct ip_pair *pair =  NULL;
 	struct pcap_dumper_t *dumper = NULL;
+	u_char *pkt = NULL;
+	char *fname = NULL;
 	unsigned short offset;
 	unsigned long src_ip, dst_ip;
 	unsigned short src_port, dst_port;
-	char *fname = NULL;
-	char *filepath = NULL;
-	unsigned short fplen = strlen(outputdir)+FILE_NAME_LENGTH+2;
 	
-	filepath = malloc(fplen);
 	while ((pkt = (u_char *)pcap_next (inputp, &hdr)) != NULL){
 		// Get IP layer information
 		ethh = (struct ether_header *)pkt;
@@ -190,40 +224,38 @@ void process_trace ()
 				continue;
 			}
 			pair = register_ip_pair (iph->ip_src.s_addr,iph->ip_dst.s_addr,src_port, dst_port);
+			if (iph->ip_p == IPPROTO_UDP)
+				pair->pdf.status = STS_UDP;
+			else{
+				if ((tcph->th_flags & TH_SYN) == TH_SYN)
+					pair->pdf.status = STS_TCP_SYN;
+				else
+					pair->pdf.status = STS_TCP_NOSYN;
+			}	
 		}
-		
-		pdf = &pair->pdf;
-		if(pdf->pkts == 0){
+
+		// Fill the ip_pair with information of the current flow
+		if(pair->pdf.pkts == 0){
 			// A new flow item reated with empty dump file object
 			fname = new_file_name(src_ip, dst_ip, src_port, dst_port, hdr.ts.tv_sec);
-			record_flow_name(fname);
-			memcpy(pdf->file_name, fname, strlen(fname));
-			pdf->start_time = hdr.ts.tv_sec;
-			pdf->pkts++;
+			memcpy(pair->pdf.file_name, fname, strlen(fname));
+			pair->pdf.start_time = hdr.ts.tv_sec;
 			free(fname);
 		}else{
-			if(hdr.ts.tv_sec - pdf->start_time >= FLOW_TIMEOUT){
+			if(hdr.ts.tv_sec - pair->pdf.start_time >= FLOW_TIMEOUT){
 				// Rest the pair to start a new flow with the same 4-tuple, but with
 				// the different name and timestamp
-				reset_pdf(pdf);
+				reset_pdf(&(pair->pdf));
 				fname = new_file_name(src_ip, dst_ip, src_port, dst_port, hdr.ts.tv_sec);
-				record_flow_name(fname);
-				memcpy(pdf->file_name, fname, strlen(fname));
-				pdf->start_time = hdr.ts.tv_sec;
-				pdf->pkts++;
+				memcpy(pair->pdf.file_name, fname, strlen(fname));
+				pair->pdf.start_time = hdr.ts.tv_sec;
 				free(fname);
 			}
 		}
 		
-		// Prepare the flow file path
-		memset(filepath, '\0', fplen);
-		strcpy(filepath, outputdir);
-		strcat(filepath, "/");
-		strcat(filepath, pdf->file_name);
-		
 		// Dump the packet to file and close the file
-		FILE *f = fopen(filepath, "ab");
-		if(pdf->pkts == 0){
+		FILE *f = fopen(resemble_file_path(&(pair->pdf)), "ab");
+		if(pair->pdf.pkts == 0){
 			// Call the pcap_dump_fopen to write the pcap file header first
 			// to the new file
 			dumper = pcap_dump_fopen(inputp, f);
@@ -234,9 +266,8 @@ void process_trace ()
 		// Dump the packet now
 		pcap_dump ((u_char *)dumper, &hdr, (unsigned char *)pkt);
 		pcap_dump_close(dumper);
+		pair->pdf.pkts++;
 	}
-	
-	free(filepath);
 }
 
 
