0c8bddeab28f5eef23f663bcc5ad4b9a63769650
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2025-08-12 14:47:07 +0800

    Update: "include/pkt2flow.h" [skip ci]

--- include/pkt2flow.h
+++ include/pkt2flow.h
@@ -1,9 +1,8 @@
-
 /* pkt2flow
- * Xiaming Chen (chen_xm@sjtu.edu.cn)
  *
- * Copyright (c) 2012
- * Copyright (c) 2014 Sven Eckelmann <sven@narfation.org>
+ * Copyright (c) 2012  Xiaming Chen <chen_xm@sjtu.edu.cn>
+ * Copyright (C) 2014  Sven Eckelmann <sven@narfation.org>
+ * Copyright (C) 2025  Xiaming Chen <chenxm35@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation files
@@ -31,6 +30,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 #ifndef PKT2FLOW_H
 #define PKT2FLOW_H
 
